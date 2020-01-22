92f43b960b42549e0feddb311e7574546d7438b4
Ross Williams <ross@ross-williams.net>
2020-01-22 09:19:47 -0500

    Working asyncio websockets class

--- awl.py
+++ awl.py
@@ -4,12 +4,16 @@ import asyncio
 import json
 import re
 import requests
+from urllib3.util.url import parse_url
 import websockets
+from autologging import logged, traced
 
 
+@logged
+@traced
 class AWL:
 
-    LOGIN_URI = "https://symphony.mywaterfurnace.com/account/login"
+    LOGIN_URI = 'https://symphony.mywaterfurnace.com/account/login'
     AWLCONFIG_URI = 'https://symphony.mywaterfurnace.com/assets/js/awlconfig.js.php'
     COMMAND_SOURCE = 'consumer dashboard'
 
@@ -20,7 +24,7 @@ class AWL:
         self.http_session = requests.Session()
         self.http_session.cookies.set(
             'legal-acknowledge', 'yes',
-            domain='symphony.mywaterfurnace.com',
+            domain=parse_url(self.LOGIN_URI).host,
             path='/'
         )
 
@@ -45,7 +49,7 @@ class AWL:
         self.websockets_connection = None
 
         self.transactions = dict()
-        self.transaction_id = None
+        self.next_transaction_id = None
         self.transaction_lock = asyncio.Lock()
 
     def __del__(self):
@@ -63,37 +67,65 @@ class AWL:
             websockets.connect(self.websockets_uri)
         )
         async with self.transaction_lock:
-            self.transaction_id = 1
+            self.next_transaction_id = 1
+        asyncio.create_task(self._receive())
+
+    async def listen(self):
+        await self.websockets_connection.wait_closed()
 
     async def close(self):
         if self.websockets_connection is not None:
             await self.websockets_connection.close()
             async with self.transaction_lock:
-                self.transaction_id = None
+                self.next_transaction_id = None
 
     @property
     def session_id(self):
         return self.http_session.cookies.get('sessionid', default=None)
 
+    async def _receive(self):
+        async for message in self.websockets_connection:
+            self.__log.debug(f"< {message}")
+            data = json.loads(message)
+            tid = data['tid']
+            try:
+                async with self.transaction_lock:
+                    self.transactions.pop(tid).set_result(data)
+            except KeyError:
+                self.__log.warning(f"< Unknown transaction id {tid}")
+
     async def _command(self, command, **kwargs):
-        payload = kwargs
         async with self.transaction_lock:
-            payload.update({
-                "cmd": command,
-                "tid": self.transaction_id,  # TODO: increment tid
-                "source": self.COMMAND_SOURCE,
-                "sessionid": self.session_id,
-            })
-            self.transactions[self.transaction_id] = command
-            self.transaction_id += 1
+            tid = self.next_transaction_id
+            self.next_transaction_id += 1
+            transaction_future = asyncio.get_running_loop().create_future()
+            self.transactions[tid] = transaction_future
+
+        payload = kwargs
+        payload.update({
+            "cmd": command,
+            "tid": tid,
+            "source": self.COMMAND_SOURCE,
+        })
         payload_json = json.dumps(payload)
-        print(f"> {payload_json}")
+        self.__log.debug(f"> {payload_json}")
         await self.websockets_connection.send(payload_json)
+        return transaction_future
+
+    async def _command_wait(self, command, **kwargs):
+        fut = await self._command(command, **kwargs)
+        ret = await fut
+        return ret
 
     async def login(self):
-        await self._command('login')
+        login_data = await self._command_wait('login', sessionid=self.session_id)
+        return login_data
 
-        # TODO: move recv to transactional event loop
-        login_response = await self.websockets_connection.recv()
-        print(f"< {login_response}")
+    async def read(self, awlid, zone=0):
+        read_data = await self._command_wait('read',
+            awlid=awlid,
+            zone=zone,
+            rlist=["compressorpower","fanpower","auxpower","looppumppower","totalunitpower","AWLABCType","ModeOfOperation","ActualCompressorSpeed","AirflowCurrentSpeed","AuroraOutputEH1","AuroraOutputEH2","AuroraOutputCC","AuroraOutputCC2","TStatDehumidSetpoint","TStatRelativeHumidity","LeavingAirTemp","TStatRoomTemp","EnteringWaterTemp","AOCEnteringWaterTemp","auroraoutputrv","AWLTStatType","humidity_offset_settings","iz2_humidity_offset_settings","dehumid_humid_sp","iz2_dehumid_humid_sp","lockoutstatus","lastfault","lastlockout","homeautomationalarm1","homeautomationalarm2","iz2_z1_roomtemp","iz2_z1_activesettings","TStatActiveSetpoint","TStatMode","TStatHeatingSetpoint","TStatCoolingSetpoint","iz2_z2_roomtemp","iz2_z2_activesettings","iz2_z3_roomtemp","iz2_z3_activesettings"]
+        )
+        return read_data
 
--- awl_toy_login.py
+++ awl_toy_login.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 import asyncio
+import logging
 import os
+from pprint import pprint
+import sys
 
 from awl import AWL
 
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
+    format="%(levelname)s:%(name)s:%(funcName)s:%(message)s")
 
 async def main():
     async with AWL(
         os.environ['WATERFURNACE_USER'],
         os.environ['WATERFURNACE_PASSWORD']
     ) as awl_connection:
-        await awl_connection.login()
+        login_data = await awl_connection.login()
+        pprint(login_data)
+        for unit in login_data['locations'][0]['gateways']:
+            pprint(await awl_connection.read(unit['gwid']))
 
 if __name__ == '__main__':
     asyncio.run(main())
