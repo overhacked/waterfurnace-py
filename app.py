7327ae3c47a7e683875bb1c5c22ee0b40fe3c27c
Ross Williams <ross@ross-williams.net>
2020-01-24 16:56:08 -0500

    Make URI structure more complete

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import asyncio
 
 import backoff
-from quart import jsonify, Quart
+from quart import abort, jsonify, Quart
 
 from awl import AWL, AWLConnectionError, AWLLoginError
 
@@ -63,18 +63,11 @@ async def close_awl_session():
     await app.awl_connection.close()
 
 
-@app.route('/gateways')
-async def list_gateways():
-    return jsonify(app.awl_connection.login_data)
+async def awl_read_gateway(gwid):
+    return await app.awl_connection.read(gwid)
 
 
-@app.route('/gateways/<gwid>')
-async def read_gateway(gwid):
-    return jsonify(await app.awl_connection.read(gwid))
-
-
-@app.route('/thermostats')
-async def list_thermostats():
+def awl_enumerate_zones():
     awl_login_data = app.awl_connection.login_data
     thermostats = list()
     for location in awl_login_data['locations']:
@@ -96,4 +89,78 @@ async def list_thermostats():
                     except KeyError:
                         app.logger.error("Couldn't get gwid")
 
-    return jsonify(thermostats)
+    return thermostats
+
+
+@app.route('/zones')
+async def list_thermostats():
+    return jsonify(awl_enumerate_zones())
+
+
+@app.route('/gateways')
+async def list_gateways():
+    return jsonify(app.awl_connection.login_data)
+
+
+@app.route('/gateways/<gwid>')
+async def read_gateway(gwid):
+    gateway_data = await awl_read_gateway(gwid)
+    return jsonify(gateway_data)
+
+
+@app.route('/gateways/<gwid>/zones')
+async def list_gateway_zones(gwid):
+    if gwid == '*':
+        return await list_thermostats()
+
+    gateway_zones = [
+        zone for
+        zone in awl_enumerate_zones()
+        if zone['gwid'] == gwid
+    ]
+    return jsonify(gateway_zones)
+
+
+@app.route('/gateways/<gwid>/zones/<int:zoneid>')
+async def view_gateway_zone(gwid, zoneid):
+    gateway_zone = [
+        zone for
+        zone in awl_enumerate_zones()
+        if zone['gwid'] == gwid and zone['zoneid'] == zoneid
+    ]
+    if len(gateway_zone) == 0:
+        abort(404)
+    if len(gateway_zone) > 1:
+        abort(500)
+    return jsonify(gateway_zone[0])
+
+
+@app.route('/gateways/<gwid>/zones/<int:zoneid>/details')
+async def read_zone(gwid, zoneid):
+    gateway_data = await awl_read_gateway(gwid)
+
+    # Find all zone-specific data
+    # in the gateway
+    zone_prefix = f"iz2_z{zoneid}_"
+    zone_raw_data = {
+        key: value for
+        (key, value) in gateway_data.items()
+        if key.startswith(zone_prefix)
+    }
+
+    # Pull e.g. $.iz2_z1_activesettings.* up
+    # to the top level
+    zone_data = dict()
+    zone_data.update(
+        zone_raw_data.pop(f"{zone_prefix}activesettings", dict())
+    )
+    zone_data.update(zone_raw_data)
+
+    # Strip the prefix
+    response_data = {
+        key.replace(zone_prefix, '', 1): value
+        for (key, value)
+        in zone_data.items()
+    }
+
+    return jsonify(response_data)
