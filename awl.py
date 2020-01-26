24d85f5086c8d9f9c814289d544853ac77d1f464
Ross Williams <ross@ross-williams.net>
2020-01-26 16:37:10 -0500

    Updated dependencies to Python 3.8 and added type annotations

--- awl.py
+++ awl.py
@@ -2,15 +2,17 @@
 
 import asyncio
 import json
+import logging
 import re
 import requests
-from urllib3.util.url import parse_url
+from requests.packages.urllib3.util.url import parse_url
+from typing import Any, Dict, Optional, Final
 import websockets
 from autologging import logged, traced
 
 
 # Default 1 hour timeout
-AWL_DEFAULT_TRANSACTION_TIMEOUT = 60*60
+AWL_DEFAULT_TRANSACTION_TIMEOUT: Final = 60*60
 
 
 class AWLLoginError(RuntimeError):
@@ -28,15 +30,16 @@ class AWLTransactionError(RuntimeError):
 @logged
 @traced
 class AWL:
+    __log: logging.Logger
 
     # Taken from setTimeout(1500000, ...) in Symphony JavaScript
-    SESSION_TIMEOUT = 1500
+    SESSION_TIMEOUT: Final = 1500
 
-    LOGIN_URI = 'https://symphony.mywaterfurnace.com/account/login'
-    AWLCONFIG_URI = \
+    LOGIN_URI: Final = 'https://symphony.mywaterfurnace.com/account/login'
+    AWLCONFIG_URI: Final = \
         'https://symphony.mywaterfurnace.com/assets/js/awlconfig.js.php'
-    COMMAND_SOURCE = 'consumer dashboard'
-    AWL_GATEWAY_RLIST = [
+    COMMAND_SOURCE: Final = 'consumer dashboard'
+    AWL_GATEWAY_RLIST: Final = [
         "ActualCompressorSpeed",
         "AirflowCurrentSpeed",
         "AOCEnteringWaterTemp",
@@ -73,17 +76,17 @@ class AWL:
         "TStatRoomTemp",
     ]
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
 
-        self.websockets_connection = None
-        self._login_data = None
-        self.receive_task = None
+        self.websockets_connection: Optional[websockets.client.WebSocketClientProtocol] = None
+        self._login_data: Optional[dict] = None
+        self.receive_task: Optional[asyncio.Task] = None
 
-        self._transaction_lock = asyncio.Lock()
-        self._transactions = dict()
-        self._transaction_id = 0
+        self._transaction_lock: Final[asyncio.Lock] = asyncio.Lock()
+        self._transactions: Final[Dict[int, asyncio.Future]] = dict()
+        self._transaction_id: int = 0
 
     def __del__(self):
         self.http_session.close()
@@ -95,7 +98,7 @@ class AWL:
     async def __aexit__(self, *excinfo):
         await self.close()
 
-    async def __next_transaction_id(self):
+    async def __next_transaction_id(self) -> int:
         async with self._transaction_lock:
             # Reset to 1 when next tid would be larger
             # than an 8-bit integer
@@ -127,7 +130,7 @@ class AWL:
             # Reset the transaction id
             self._transaction_id = 0
 
-    async def __start_transaction(self, tid, timeout):
+    async def __start_transaction(self, tid: int, timeout: int) -> asyncio.Future:
         async with self._transaction_lock:
             transaction_future = asyncio.get_running_loop().create_future()
             self._transactions[tid] = transaction_future
@@ -137,7 +140,7 @@ class AWL:
             )
         return transaction_future
 
-    async def __commit_transaction(self, tid, data):
+    async def __commit_transaction(self, tid: int, data: Any):
         try:
             async with self._transaction_lock:
                 self._transactions.pop(tid).set_result(data)
@@ -146,7 +149,7 @@ class AWL:
                 f"< Unknown transaction id {tid}: {data!r}"
             )
 
-    async def __abort_transaction(self, tid, err=None):
+    async def __abort_transaction(self, tid: int, err: Optional[str] = None):
         try:
             async with self._transaction_lock:
                 self._transactions.pop(tid).set_exception(
@@ -201,7 +204,7 @@ class AWL:
         except requests.HTTPError:
             raise AWLLoginError(f"Logout failed: {logout_response.reason}")
 
-    def __get_websockets_uri(self):
+    def __get_websockets_uri(self) -> str:
         wssuri_response = self.http_session.get(self.AWLCONFIG_URI)
         try:
             wssuri_response.raise_for_status()
@@ -220,7 +223,7 @@ class AWL:
             )
         return websockets_uri_matches[0]
 
-    async def __websockets_connect(self, websockets_uri):
+    async def __websockets_connect(self, websockets_uri: str):
         try:
             self.websockets_connection = await (
                 websockets.connect(websockets_uri)
@@ -231,7 +234,7 @@ class AWL:
             )
         except websockets.InvalidURI:
             raise AWLLoginError(
-                f"Invalid websockets URI: {self.websockets_uri}"
+                f"Invalid websockets URI: {websockets_uri}"
             )
         self.receive_task = asyncio.create_task(self.__websockets_receive())
 
@@ -265,9 +268,12 @@ class AWL:
         )
         return self._login_data
 
-    async def _command(self, command,
-                       transaction_timeout=AWL_DEFAULT_TRANSACTION_TIMEOUT,
-                       **kwargs):
+    async def _command(self, command: str,
+                       transaction_timeout: int = AWL_DEFAULT_TRANSACTION_TIMEOUT,
+                       **kwargs) -> asyncio.Future:
+        if self.websockets_connection is None:
+            raise AWLConnectionError(f"Call {__name__}.connect() before making requests")
+
         tid = await self.__next_transaction_id()
 
         payload = kwargs
@@ -286,7 +292,7 @@ class AWL:
         await self.websockets_connection.send(payload_json)
         return transaction_future
 
-    async def _command_wait(self, command, **kwargs):
+    async def _command_wait(self, command: str, **kwargs):
         fut = await self._command(command, **kwargs)
         try:
             ret = await fut
@@ -332,21 +338,24 @@ class AWL:
             pass
 
     @property
-    def session_id(self):
+    def session_id(self) -> Optional[str]:
         return self.http_session.cookies.get('sessionid', default=None)
 
     @property
     def login_data(self):
         return self._login_data
 
-    def get_gwid_param(self, gwid, param):
-        for location in self._login_data.get('locations'):
-            for gateway in location.get('gateways'):
+    def get_gwid_param(self, gwid: str, param: str) -> Any:
+        if self._login_data is None:
+            return
+
+        for location in self._login_data.get('locations', list()):
+            for gateway in location.get('gateways', list()):
                 if gateway.get('gwid') == gwid:
                     return gateway.get(param)
 
-    async def read(self, awlid, zone=0,
-                   timeout=AWL_DEFAULT_TRANSACTION_TIMEOUT):
+    async def read(self, awlid: str, zone: int = 0,
+                   timeout: int = AWL_DEFAULT_TRANSACTION_TIMEOUT) -> Any:
         read_rlist = self.AWL_GATEWAY_RLIST.copy()
 
         max_zones = self.get_gwid_param(awlid, 'iz2_max_zones')
